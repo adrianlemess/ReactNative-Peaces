e32c1e89fc36d8713c15456d043b7ca0643182e1
Adrian <502712508@logon.ds.ge.com>
2017-05-30 19:09:34 -0300

    add more redux logic to userLogin

--- manager/src/App.js
+++ manager/src/App.js
@@ -1,18 +1,30 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import firebase from 'firebase';
+import ReduxThunk from 'redux-thunk';
 import reducers from './reducers';
+import LoginForm from './components/LoginForm';
 
 class App extends Component {
+    componentWillMount() {
+        const config = {
+            apiKey: 'AIzaSyDQt9bDAuJ9UO-I0vp2vB8miRD_jYRsiFE',
+            authDomain: 'manager-edf83.firebaseapp.com',
+            databaseURL: 'https://manager-edf83.firebaseio.com',
+            projectId: 'manager-edf83',
+            storageBucket: 'manager-edf83.appspot.com',
+            messagingSenderId: '664025211515'
+        };
+
+        firebase.initializeApp(config);
+    }
     render() {
+        const store = createStore(reducers, {}, applyMiddleware(ReduxThunk));
+        
         return (
-            <Provider store={createStore(reducers)}>
-                <View>
-                    <Text>
-                        Hello!
-                    </Text>
-                </View>
+            <Provider store={store}>
+                <LoginForm />
             </Provider>
         );
     }
