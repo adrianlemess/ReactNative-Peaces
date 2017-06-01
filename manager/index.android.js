787aad5f11bc22a10a20702408309177e05c5d2d
Adrian <502712508@logon.ds.ge.com>
2017-06-01 10:13:16 -0300

    delete manager 1 for problems

--- manager/index.android.js
+++
@@ -1,6 +0,0 @@
-import { AppRegistry, UIManager } from 'react-native';
-import App from './src/App';
-
-UIManager.setLayoutAnimationEnabledExperimental(true);
-AppRegistry.registerComponent('manager', () => App);
-
